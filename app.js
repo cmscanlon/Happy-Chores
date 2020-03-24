ec7daafa830d9ea4363a3ecfb29cd2efe1a07004
cmscanlon <cmscanlon@yahoo.com>
2020-03-24 09:21:33 -0500

    Added coming soon page and npm start command -CMS

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Happy app listening on port ${port}!`))
+
+const startServerAndTest = require("start-server-and-test")
