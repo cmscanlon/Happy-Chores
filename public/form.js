543a8c90d7f675d0b8d97205036a7d57cce8029e
cmscanlon <cmscanlon@yahoo.com>
2020-04-09 09:20:59 -0500

    Started styling and HTML for all pages -CMS

--- public/form.js
+++ public/form.js
@@ -3,30 +3,30 @@ const suButton = document.getElementById('submit');
 const checkbox = document.getElementById('accept');
 
 function formValidation() {
+
   if (validateEmail(email)) {}
   return false;
 
   if(activeSubmit(suButton)) {}
     return false;
-
 }
 
-//TODO: rewrite validateEmail without using regular expressions
 function isValid(email) {
-
 const atSymbol = email.indexOf('@');
+
   if (atSymbol < 1) return false;
 
 const dot = email.indexOf('.');
-    if(dot <= atSymbol + 2) return false;
-    if (dot === email.length - 1) return false;
+
+  if(dot <= atSymbol + 2) return false;
+  if (dot === email.length - 1) return false;
 
 return true;
-  }
+}
 
 function validateEmail() {
-    const email = document.getElementById('email').value;
-    const error = document.getElementById('error');
+const email = document.getElementById('email').value;
+const error = document.getElementById('error');
     error.innerHTML = '';
     if (!isValid(email)) {
         error.innerHTML = "Please enter a valid email address.";
@@ -40,4 +40,24 @@ function activeSubmit() {
     suButton.disabled = true;
   }
 }
+
+//function clearFields() {
+//    const elements = oForm.elements;
+//
+//    for (i = 0; i < elements.length; i++) {
+//        fieldType = elements[i].type.toLowerCase();
+//        switch (fieldType) {
+//        case 'text':
+//            elements[i].value = '';
+//            break;
+//        case 'checkbox':
+//            if (elements[i].checked) {
+//                elements[i].checked = false;
+//            }
+//            break
+//        default:
+//            break;
+//        }
+//    }
+
 
