1cd111eb5b10b4179d09674dfe620b2ee3c2325c
Carolyn <cmscanlon@yahoo.com>
2020-04-29 17:33:44 -0500

    Created chore page, JS file, and added basic style. -CMS

---
+++ public/chore.js
@@ -0,0 +1,19 @@
+function clearFields() {
+    const elements = document.getElementById('chore-form').elements;
+
+    for (i = 0; i < elements.length; i++) {
+        fieldType = elements[i].type.toLowerCase();
+        switch (fieldType) {
+        case 'text':
+            elements[i].value = '';
+            break;
+        case 'checkbox':
+            if (elements[i].checked) {
+                elements[i].checked = false;
+            }
+            break
+        default:
+            break;
+        }
+    }
+}
