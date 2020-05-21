344b7e2f772ebfc9fe0dbe07b55a4e381bf1bc87
Carolyn <cmscanlon@yahoo.com>
2020-05-21 14:04:56 -0500

    Delete chore functionality -CMS

--- public/chore.js
+++ public/chore.js
@@ -5,9 +5,11 @@ const chore = document.getElementById('chore');
 const assign = document.getElementById('assigned');
 const suButton = document.getElementById('submit');
 
-app.addEventListener('click', function(c) {
-    this.removeChild(c.target);
-})
+// app.addEventListener('click', function(c) {
+//     // this.removeChild(c.target);
+//     console.log(c.target);
+// })
+
 
 function emptyFields() {
     if (chore.value !== '' && assign.value !== '') {
@@ -33,17 +35,36 @@ choreList.forEach(function (chore) {
 });
 
 function addChore() {
+    const button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'delete');
+    button.setAttribute('value', 'Delete');
+    button.setAttribute('onclick', 'deleteChore(event)');
+
     const newLi = document.createElement('li');
+    newLi.setAttribute('class', 'chore-li');
+    newLi.setAttribute('id', 'chores')
     const newChore = {
         name: document.getElementById('chore').value,
         assignee: document.getElementById('assigned').value
     }
     newLi.textContent = `Chore: ${newChore.name} - Assigned to: ${newChore.assignee}`;
+    
+    newLi.appendChild(button);
     choreList.push(newChore);
     app.appendChild(newLi);
     clearFields();
     return false; 
 }
+
+function deleteChore(event) {
+    console.log(event.target.closest('li'));
+    const chores = document.getElementById('chores');
+    if (chores.parentNode) {
+        chores.parentNode.removeChild(event.target.closest('li'));
+    }
+}
+
 
 function clearFields() {
     const elements = document.getElementById('chore-form').elements;
