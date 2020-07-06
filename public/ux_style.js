bf8106ab0577e61ac01dd622b205af927d08bd0e
Carolyn <cmscanlon@yahoo.com>
2020-07-06 17:56:26 -0500

    Style looks like design -CMS

--- public/ux_style.js
+++ public/ux_style.js
@@ -1,7 +1,17 @@
-function overlayOn() {
-    document.getElementsById("overlay").style.display = "block";
+const modal = document.getElementById("overlay");
+const btn = document.getElementById("add-chore");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modual.style.display = "none";
+    }
 }
